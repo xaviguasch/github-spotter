d381d4f03003618b691ef00aac2a681c41fdb6da
xaviguasch <xaviguasch@gmail.com>
2019-06-05 11:31:42 +0200

    added UserItem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Navbar icon='fab fa-github' />
+        <Navbar title='Github Spotter' icon='fab fa-github' />
+        <UserItem />
       </div>
     )
   }
