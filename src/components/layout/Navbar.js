a3bbc38a78a6fbad55807d39d354cb75a9cc374c
xaviguasch <xaviguasch@gmail.com>
2019-06-05 10:52:03 +0200

    toyed with propTypes and static defaultProps

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Navbar extends Component {
+  static defaultProps = {
+    title: 'Github Finder -from defaultProps-'
+  }
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon} /> {this.props.title}
+        </h1>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
