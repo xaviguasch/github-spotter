7e7cc59fd6799073385b46dff7374286eb039298
xaviguasch <xaviguasch@gmail.com>
2019-06-05 12:31:33 +0200

    converted Navbar and Users components to functional ones

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class Navbar extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  }
+const Navbar = props => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={props.icon} /> {props.title}
+      </h1>
+    </nav>
+  )
+}
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    )
-  }
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
 }
 
 export default Navbar
