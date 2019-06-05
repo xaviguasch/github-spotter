b5746f05552e6bc1c36a0ba38566c1a9d9c7a8f6
xaviguasch <xaviguasch@gmail.com>
2019-06-05 19:42:01 +0200

    added the Search component with a form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 import './App.css'
 
@@ -17,7 +18,11 @@ class App extends Component {
   async componentDidMount() {
     this.setState({ loading: true })
 
-    const res = await axios.get('https://api.github.com/users')
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
 
     this.setState({ users: res.data, loading: false })
   }
@@ -27,6 +32,7 @@ class App extends Component {
       <div className='App'>
         <Navbar title='Github Spotter' icon='fab fa-github' />
         <div className='container'>
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
