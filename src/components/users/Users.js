120fb6bd667a82abbf69587ea7370f38a364178b
xaviguasch <xaviguasch@gmail.com>
2019-06-05 11:54:20 +0200

    dynamically generated individual users from the Users component

---
+++ src/components/users/Users.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import UserItem from './UserItem'
+
+class Users extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      users: [
+        {
+          id: '1',
+          login: 'mojombo',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+          html_url: 'https://github.com/mojombo'
+        },
+        {
+          id: '2',
+          login: 'defunkt',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+          html_url: 'https://github.com/defunkt'
+        },
+        {
+          id: '3',
+          login: 'pjhyett',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
+          html_url: 'https://github.com/pjhyett'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Users component</h1>
+        {this.state.users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default Users
