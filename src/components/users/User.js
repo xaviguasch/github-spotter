332cfdf214d0528a79147041eca26f40ccfcd67e
xaviguasch <xaviguasch@gmail.com>
2019-06-06 20:59:59 +0200

    completed user component UI

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -17,6 +17,7 @@ export class User extends Component {
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -36,7 +37,6 @@ export class User extends Component {
 
     return (
       <Fragment>
-        <h1>{name}</h1>
         <Link to='/' className='btn btn-light'>
           Back to Search
         </Link>
@@ -48,6 +48,59 @@ export class User extends Component {
             <i className='fas fa-times-circle text-danger' />
           )}
         </p>
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div className=''>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Followers: {followers}</div>
+        </div>
       </Fragment>
     )
   }
