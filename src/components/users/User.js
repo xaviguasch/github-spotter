9516f40569266fbd9a29f5c8d937e7e855b7df8d
xaviguasch <xaviguasch@gmail.com>
2019-06-06 13:39:41 +0200

    added Link back to home in User and 'hireable' checkmark

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login)
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  }
+
   render() {
     const {
       name,
@@ -21,7 +31,25 @@ export class User extends Component {
     } = this.props.user
 
     const { loading } = this.props
-    return <div>{name}</div>
+
+    if (loading) return <Spinner />
+
+    return (
+      <Fragment>
+        <h1>{name}</h1>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        <p>
+          Hireable: {''}
+          {hireable ? (
+            <i className='fas fa-check text-success' />
+          ) : (
+            <i className='fas fa-times-circle text-danger' />
+          )}
+        </p>
+      </Fragment>
+    )
   }
 }
 
